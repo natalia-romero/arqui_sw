5cd8d146ed17bfab7304147cdeb9d4d90c135dce
Natalia Romero <natalia.romero_g@mail.udp.cl>
2024-06-07 14:55:12 -0400

    se agregan servicios por separado

---
+++ services/auth.go
@@ -0,0 +1,64 @@
+package services
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+type Service2 struct {
+	service1 *Service1
+}
+
+func (s *Service2) Execute() bool {
+	s.service1.Execute()
+	return s.authenticateUser()
+}
+
+func (s *Service2) authenticateUser() bool {
+	var username, password string
+
+	fmt.Print("Ingrese nombre de usuario: ")
+	fmt.Scanln(&username)
+	fmt.Print("Ingrese contraseña: ")
+	fmt.Scanln(&password)
+
+	var storedPassword string
+	err := s.service1.db.QueryRow("SELECT password FROM users WHERE username = ?", username).Scan(&storedPassword)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			fmt.Println("Usuario no encontrado.")
+		} else {
+			log.Fatal(err)
+		}
+		return false
+	}
+
+	if password == storedPassword {
+		var status int
+		err2 := s.service1.db.QueryRow("SELECT status_id FROM users WHERE username = ? AND password = ?", username, password).Scan(&status)
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+
+		if status == 1 {
+			fmt.Println("Inicio de sesión exitoso.")
+			return true
+		} else {
+			fmt.Println("Usuario inactivo.")
+			return false
+		}
+	} else {
+		fmt.Println("Contraseña incorrecta.")
+		return false
+	}
+}
+
+func handleService2(data string) string {
+	service1 := &Service1{}
+	service2 := &Service2{service1: service1}
+	if service2.Execute() {
+		return fmt.Sprintf("%05d%s%sOK%s", len(data)+10, "auth", data, " Autenticación exitosa")
+	}
+	return fmt.Sprintf("%05d%s%sNK%s", len(data)+8, "auth", data, " Autenticación fallida")
+}
---
+++ services/db.go
@@ -0,0 +1,51 @@
+package services
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Service1 struct {
+	User string
+	Pass string
+	db   *sql.DB
+}
+
+func (s *Service1) Execute() {
+	s.User = os.Getenv("DBUSER")
+	s.Pass = os.Getenv("DBPASS")
+	dsn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/arqui_sw?charset=utf8&parseTime=True&loc=Local", s.User, s.Pass)
+	var err error
+	s.db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	pingErr := s.db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Conectado a la base de datos")
+	res, err := s.db.Query("SHOW TABLES")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Close()
+
+	var table string
+	fmt.Println("Listado de tablas:")
+	for res.Next() {
+		res.Scan(&table)
+		fmt.Println(" - ", table)
+	}
+}
+
+func handleService1(data string) string {
+	service1 := &Service1{}
+	service1.Execute()
+	// Simulación de procesamiento de datos
+	return fmt.Sprintf("%05d%s%sOK%s", len(data)+10, "serv1", data, " Operación exitosa")
+}
