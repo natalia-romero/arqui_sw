ef47d586c24ab18291d950be02830c4c7e2cc08b
Natalia Romero <natalia.romero_g@mail.udp.cl>
2024-06-16 22:32:36 -0400

    se agrega cliente

---
+++ clients/admin.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/natalia-romero/arqui_sw/services"
+)
+
+func generateConnection() {
+	serviceName := "serv1"
+	data := "basededatos"
+	response, err := services.SendToSOABus(serviceName, data)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("Response from SOA bus:", response)
+	if strings.Contains(response, "OK") {
+		fmt.Println("Conectando a la base de datos...")
+		services.HandleService1()
+	} else {
+		fmt.Println("No se puede conectar a la base de datos.")
+	}
+
+}
+
+func main() {
+	generateConnection()
+}
